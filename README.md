8aab5155d55e3ea233e4882810b793e230b51121
jesuschicano <jesuschicano3988@gmail.com>
2018-02-05 19:45:15 +0100

    Primera página en Simfony

---
+++ README.md
@@ -0,0 +1,4 @@
+demo-symfony
+============
+
+A Symfony project created on January 29, 2018, 1:18 pm.
---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('lucky/number.html.twig', array(
+          'number' => $number,
+        ));
+    }
+}
